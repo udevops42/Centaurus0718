b57283b76a8260815c8fc6d8e8871603758e6869
sapsun <sapsun@centaurus.com>
2018-08-08 13:42:16 -0500

    I added scripts in SunilBranch

--- scripts/shellscripts/sunilAssignments/hw1.sh
+++ scripts/shellscripts/sunilAssignments/hw1.sh
@@ -3,11 +3,11 @@ version=$( cat /etc/redhat-release | awk '{ print $3 }' | cut -d "." -f1 )
 
 if [ $version -eq 6 ]
 then
-	yum install -y httpd
+        yum install -y httpd
 elif [ $version -eq 7 ]
 then
-	yum install -y wget
+        yum install -y wget
 else
-	echo "This release is other than 6 or 7"
+        echo "This release is other than 6 or 7"
 fi
-	
+
