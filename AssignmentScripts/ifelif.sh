b089125418534ba5327feeb88913c0364c88b9ec
root <root@prabesh.devops.class>
2018-08-09 08:05:15 -0500

    successful implementation of ifelif file

--- AssignmentScripts/ifelif.sh
+++ AssignmentScripts/ifelif.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 #If the OS is CentOS 6, install httpd, else if OS is CentOS 7, install wget
 
+cat /etc/redhat-release
 echo "Hi, user!"
-centver="cat /etc/centos-release"
-
+centver=$(cat /etc/redhat-release)
 echo "Your CentOS version is $centver"
-
