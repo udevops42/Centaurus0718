d6713b952aef390a3b00cfc3eae0e98e43be3320
root <root@prabesh.devops.class>
2018-08-11 13:39:49 -0500

    successful code implementation

--- AssignmentScripts/ifelif.sh
+++ AssignmentScripts/ifelif.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 #If the OS is CentOS 6, install httpd, else if OS is CentOS 7, install wget
 
-cat /etc/redhat-release
 echo "Hi, user!"
-centver=$(cat /etc/redhat-release)
+centver=$(cat /etc/redhat-release | awk '{print $3}' | awk 'BEGIN {FS="."} {print $1}')
 echo "Your CentOS version is $centver"
+if [ $centver == 6 ]
+then
+echo "HTTPD will be installed in your machine."
+yum install -y  httpd
+else [ $centver == 7 ]
+echo "WGET will be installed in your machine."
+yum install -y wget
+fi
+
